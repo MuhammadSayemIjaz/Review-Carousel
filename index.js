f1910c71965b5113c57b4c34ded9de2ed4a36455
Muhammad Sayem Ijaz <75717799+MuhammadSayemIjaz@users.noreply.github.com>
2021-12-13 10:58:07 +0500

    Creating And Styling Web Page

---
+++ index.js
@@ -0,0 +1,34 @@
+const profiles = [
+    {
+     id: 1,
+     name: "Susan Smith",
+     job: "Web Developer",
+     img:"imgaes/bgimg-01.jpeg",
+     desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas voluptates fugiat sed error, eos quaerat saepe eligendi, unde veritatis, sint possimus ad? Facilis, odit temporibus reprehenderit nulla doloribus officia nostrum.",
+    },
+    {
+    id: 2,
+    name: "John Doe",
+    job: "Back End Developer",
+    img:"imgaes/bgimg-01.jpeg",
+    desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas voluptates fugiat sed error, eos quaerat saepe eligendi, unde veritatis, sint possimus ad? Facilis, odit temporibus reprehenderit nulla doloribus officia nostrum.",
+    },
+    {
+    id: 3,
+     name: "Oliver",
+     job: "Front End Developer",
+     img:"imgaes/bgimg-01.jpeg",
+     desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas voluptates fugiat sed error, eos quaerat saepe eligendi, unde veritatis, sint possimus ad? Facilis, odit temporibus reprehenderit nulla doloribus officia nostrum.",
+    },
+    {
+    id: 4,
+     name: "James",
+     job: "Web Designer",
+     img:"imgaes/bgimg-01.jpeg",
+     desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas voluptates fugiat sed error, eos quaerat saepe eligendi, unde veritatis, sint possimus ad? Facilis, odit temporibus reprehenderit nulla doloribus officia nostrum.",
+    }
+];
+console.log(profiles[1]);
+console.log(profiles[2]);
+console.log(profiles[3]);
+console.log(profiles[4]);
