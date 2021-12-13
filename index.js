cb8b85979a551b33172114dc11f4554b8d7ee0c8
Muhammad Sayem Ijaz <75717799+MuhammadSayemIjaz@users.noreply.github.com>
2021-12-13 12:57:17 +0500

    Adding Images and Functioning in index.html web page

--- index.js
+++ index.js
@@ -1,34 +1,85 @@
+
 const profiles = [
     {
      id: 1,
      name: "Susan Smith",
      job: "Web Developer",
-     img:"imgaes/bgimg-01.jpeg",
+     img:  "images/bgimg-01.jpeg",
      desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas voluptates fugiat sed error, eos quaerat saepe eligendi, unde veritatis, sint possimus ad? Facilis, odit temporibus reprehenderit nulla doloribus officia nostrum.",
     },
     {
     id: 2,
     name: "John Doe",
     job: "Back End Developer",
-    img:"imgaes/bgimg-01.jpeg",
-    desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas voluptates fugiat sed error, eos quaerat saepe eligendi, unde veritatis, sint possimus ad? Facilis, odit temporibus reprehenderit nulla doloribus officia nostrum.",
+    img: "images/Bg-image-03.jpg",
+    desc:"Consectetur adipisicing elit. Voluptas voluptates fugiat sed error, eos quaerat saepe eligendi, unde veritatis, sint possimus ad? Facilis, odit temporibus reprehenderit nulla doloribus officia nostrum.",
     },
     {
     id: 3,
      name: "Oliver",
      job: "Front End Developer",
-     img:"imgaes/bgimg-01.jpeg",
-     desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas voluptates fugiat sed error, eos quaerat saepe eligendi, unde veritatis, sint possimus ad? Facilis, odit temporibus reprehenderit nulla doloribus officia nostrum.",
+     img:"images/bgimg-01.jpeg",
+     desc:"Dolor, sit amet consectetur adipisicing elit. Voluptas voluptates fugiat sed error, eos quaerat saepe eligendi, unde veritatis, sint possimus ad? Facilis, odit temporibus reprehenderit nulla doloribus officia nostrum.",
     },
     {
     id: 4,
      name: "James",
      job: "Web Designer",
-     img:"imgaes/bgimg-01.jpeg",
-     desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas voluptates fugiat sed error, eos quaerat saepe eligendi, unde veritatis, sint possimus ad? Facilis, odit temporibus reprehenderit nulla doloribus officia nostrum.",
+     img:"images/Bg-image-03.jpg",
+     desc:"Sit amet consectetur adipisicing elit. Voluptas voluptates fugiat sed error, eos quaerat saepe eligendi, unde veritatis, sint possimus ad? Facilis, odit temporibus reprehenderit nulla doloribus officia nostrum.",
     }
 ];
-console.log(profiles[1]);
-console.log(profiles[2]);
-console.log(profiles[3]);
-console.log(profiles[4]);
+
+// Get Values From Webpage
+
+const img = document.getElementById("proImage");
+const UserName = document.getElementById("Name"); 
+const Profession = document.getElementById("Profession");
+const Description = document.getElementById("description");
+
+
+const ForwardBtn = document.querySelector(".forwardBtn");
+const PreviousBtn = document.querySelector(".previousBtn");
+const RandomBtn = document.querySelector(".randomBtn");
+
+let CurrentItem = 0;
+
+window.addEventListener('DOMContentLoaded', function () {
+   ShowProfiles(CurrentItem); 
+});
+
+function ShowProfiles(items) {
+    const item = profiles[items];
+    img.src = item.img;
+    UserName.textContent = item.name;
+    Profession.textContent = item.job; 
+    Description.textContent = item.desc;
+    console.log(item);
+};
+
+ForwardBtn.addEventListener("click" , function () {
+
+    CurrentItem++;
+    if(CurrentItem > profiles.length -1){
+
+        CurrentItem = 0 ;
+    }
+    ShowProfiles(CurrentItem);
+});
+PreviousBtn.addEventListener("click" , function () {
+
+    CurrentItem--;
+
+    if(CurrentItem < 0){
+
+        CurrentItem = profiles.length -1 ;
+    }
+    ShowProfiles(CurrentItem);
+});
+
+RandomBtn.addEventListener("click", function () {
+    
+    const RandomNumber = Math.floor(Math.random() * 3);
+    ShowProfiles(RandomNumber);
+    
+});
